083b8b77c902ae33d71a868a8ecb4661693f59e3
NicolasComin <193521@upf.br>
2026-03-29 19:02:13 -0300

    Filtro valores negativos

--- etl.py
+++ etl.py
@@ -4,49 +4,57 @@ from banco.ConectaDB import engine
 
 arquivos = glob.glob("dataset/Fatura_*.csv")
 
-#print("Arquivos encontrados:", arquivos)
-
 dfs = []
 
 for arquivo in arquivos:
-
-    df = pd.read_csv(
+    df_temp = pd.read_csv(
         arquivo,
         sep=";",
-        encoding="utf-8"
+        encoding="utf-8",
     )
+    dfs.append(df_temp)
 
-    dfs.append(df)
+df = pd.concat(dfs, ignore_index=True)
 
-df = pd.concat(dfs)
-
+# Converter data
 df["Data de Compra"] = pd.to_datetime(
     df["Data de Compra"],
-    format="%d/%m/%Y"
+    format="%d/%m/%Y",
+    dayfirst=True,
+    errors="coerce"
 )
 
+# Converter valor
 df["Valor (em R$)"] = (
     df["Valor (em R$)"]
     .astype(str)
-    .str.replace(",", ".")
-    .astype(float)
+    .str.replace(",", ".", regex=False)
 )
 
-def parse_parcela(p):
+df["Valor (em R$)"] = pd.to_numeric(
+    df["Valor (em R$)"],
+    errors="coerce"
+)
 
+# Filtro de valores negativos
+df = df[df["Valor (em R$)"] >= 0].copy()
+
+# Função de parcelas
+def parse_parcela(p):
     if p == "Única":
-        return 1,1
+        return 1, 1
 
     if "/" in str(p):
-        x,y = p.split("/")
-        return int(x),int(y)
+        x, y = p.split("/")
+        return int(x), int(y)
 
-    return None,None
+    return None, None
 
-df[["num_parcela","total_parcelas"]] = df["Parcela"].apply(
+df[["num_parcela", "total_parcelas"]] = df["Parcela"].apply(
     lambda x: pd.Series(parse_parcela(x))
 )
 
+# Enviar pro banco
 df.to_sql(
     "staging_transacoes",
     engine,
